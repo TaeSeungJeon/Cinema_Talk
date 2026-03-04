62118608343fc1d5aceb59fc01927ab6112f6d28
sksqlrlek <sksqlrlek@gmail.com>
2026-03-04 18:08:58 +0900

    feat: 홈 인덱스 영화 추천 로직 개선 및 개인화 추천 추가

--- src/main/java/Controller/IndexController.java
+++ src/main/java/Controller/IndexController.java
@@ -14,6 +14,7 @@ import Service.Vote.VoteServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
 public class IndexController implements Action {
 
@@ -27,7 +28,6 @@ public class IndexController implements Action {
 
 		response.setContentType("text/html;charset=UTF-8");
 		HttpSession session = request.getSession();
-
 		Object memNoObj = session.getAttribute("memNo");
 
 		int memNo = (memNoObj instanceof Integer) ? (Integer) memNoObj : -1;//로그인한 상태면 회원 번호 구함
@@ -35,14 +35,18 @@ public class IndexController implements Action {
 		String todayKey = "visited_" + java.time.LocalDate.now();
 
 	    if (session.getAttribute(todayKey) == null) {
-
 	        homeService.increaseTodayDau();
-
 	        session.setAttribute(todayKey, true);
 	    }
-	    
-	  List<MovieRecResponse> indexTrendMovieList = homeService.getIndexTrendList();
-	  List<MovieRecResponse> indexGenreMovieList = homeService.getIndexGenreList(memNo);
+        List<MovieRecResponse> indexTrendMovieList = homeService.getIndexTrendList();
+
+	    List<MovieRecResponse> indexGenreMovieList;
+
+	    if (memNo != -1) {
+	        indexGenreMovieList = homeService.getIndexGenreList(memNo); // 개인 선호 장르
+	    } else {
+	        indexGenreMovieList = homeService.getIndexGenreList(); // 랜덤
+	    }
 	   
 	  request.setAttribute("indexTrendMovieList", indexTrendMovieList);
 	  request.setAttribute("homeGenreMovieList", indexGenreMovieList);
